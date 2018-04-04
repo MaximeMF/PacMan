f9360d920f5e26b08a3a767568376a2ed5564686
MaximeF <fumelmax@gmail.com>
2018-04-04 09:44:18 +0200

    Add javadoc

--- src/data/IPlateau.java
+++ src/data/IPlateau.java
@@ -1,21 +1,65 @@
 package data;
 
+/**
+ * Interface of Plateau class to use in Logic package.
+ * @author Robin Algier - Maxime Mathis--Fumel - Yassin Ourkia
+ */
 public interface IPlateau {
+	
+	/**
+	 * Gets the score.
+	 * @return an integer representing the score
+	 */
 	public int getScore();
 	
-	public void setScore(int score);
+	/**
+	 * Increases the score.
+	 * @param score an integer representing the value to add to the score
+	 */
+	public void increaseScore(int score);
 	
+	/**
+	 * Gets the level.
+	 * @return an integer representing the level
+	 */
 	public int getLevel();
 	
-	public void setLevel(int level);
+	/**
+	 * Increments the level.
+	 */
+	public void incrementsLevel();
 	
+	/**
+	 * Gets the speed.
+	 * @return an integer representing the speed
+	 */
 	public int getSpeed();
 	
+	/**
+	 * Sets the speed.
+	 * @param speed an integer representing the new speed
+	 */
 	public void setSpeed(int speed);
 	
+	/**
+	 * Gets PacMan.
+	 * @return PacMan
+	 */
 	public Pacman getPlayer();
 	
+	/**
+	 * Gets a Ghost.
+	 * @param id an id representing the Ghost to get
+	 * @return the Ghost corresponding to the id
+	 */
 	public Ghost getGhost(int id);
 	
-	public int[][] board = new int[0][0];
+	/**
+	 * Changes a cell value.
+	 * @param x the abscissa of the cell
+	 * @param y the ordinate of the cell
+	 * @param value the new value of the cell
+	 */
+	public void changeCell(int x, int y, int value);
+
 }
