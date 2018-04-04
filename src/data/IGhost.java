da0860f3d68fef0fd605a23b26e4936a929ad809
killingbeta <robi61@sfr.fr>
2018-04-04 09:25:54 +0200

    Interface Ghost et classe Pacman

---
+++ src/data/IGhost.java
@@ -0,0 +1,13 @@
+package data;
+
+public interface IGhost {
+	public int getPoints();
+	
+	public void setPoints();
+	
+	public int getId();
+	
+	public int getState();
+	
+	public void setState();
+}
---
+++ src/data/Pacman.java
@@ -0,0 +1,17 @@
+package data;
+
+public class Pacman implements IPacman {
+
+	@Override
+	public int getLives() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void die() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
