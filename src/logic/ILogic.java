b3f20d1bdf3b559ea49d6ad62247233af432fe87
killingbeta <robi61@sfr.fr>
2018-04-08 19:58:20 +0200

    Update doc and methods in ILogic
    Signed-off-by: killingbeta <robi61@sfr.fr>

--- src/logic/ILogic.java
+++ src/logic/ILogic.java
@@ -7,59 +7,85 @@ import data.*;
 /**
  * Interface of Logic class to use in View package.
  * @author Robin Algier - Maxime Mathis--Fumel - Yassin Ourkia
- * @inv pacman.lives >= 0
  */
 public interface ILogic {
+	
+	/**
+	 * Tests whether the ghost can move in the given direction
+	 * @param type the type of the ghost
+	 * @param dir the direction the ghost should move towards
+	 * @return true if the ghost can move in the given direction, false otherwise
+	 */
+	public boolean canMoveGhost(GhostType type, Direction dir);
+	
+	/**
+	 * Tests whether PacMan can move in the given direction
+	 * @param dir the direction PacMan should move towards
+	 * @return true if PacMan can move in the given direction, false otherwise
+	 */
+	public boolean canMovePlayer(Direction dir);
 	
 	/**
 	 * Moves a Ghost
 	 * @param type the type of the ghost to move
 	 * @param dir the direction the ghost should move towards
-	 * @return true if the ghost succeeded to move, false otherwise
 	 * @post 0 <= ghost.x < boardWidth && 0 <= ghost.y < boardHeight
 	 */
-	public boolean moveGhost(GhostType type, Direction dir);
+	public void moveGhost(GhostType type, Direction dir);
 	
 	/**
 	 * Moves PacMan
 	 * @param dir the direction the ghost should move towards
-	 * @return true if PacMan succeeded to move, false otherwise
 	 * @post 0 <= pacman.x < boardWidth && 0 <= pacman.y < boardHeight
 	 */
-	public boolean movePlayer(Direction dir);
+	public void movePlayer(Direction dir);
 	
 	/**
-	 * @see IDataLoader#getBoard()
+	 * Gets the board.
+	 * @return the matrix of the board
+	 * @post board[x][y] != null for any x,y < boardWidth, boardHeight
 	 */
 	public Entity[][] getBoard();
 	
 	/**
-	 * @see IDataLoader#getBoardHeight()
+	 * Gets the height of the board.
+	 * @return the board height
+	 * @post boardHeight >= 5
 	 */
 	public int getBoardHeight();
 	
 	/**
-	 * @see IDataLoader#getBoardWidth()
+	 * Gets the width of the board.
+	 * @return the board width
+	 * @post boardWidth >= 5
 	 */
 	public int getBoardWidth();
 	
 	/**
-	 * @see IDataLoader#getPacmanPosition()
+	 * Gets the position of Pacman on the board.
+	 * @return the Pacman position
+	 * @post pacmanPosition.length = 2 && 0 <= pacmanPosition[0] < boardWidth && 0 <= pacmanPosition[1] < boardHeight
 	 */
 	public int[] getPacmanPosition();
 	
 	/**
-	 * @see IDataLoader#getGhostsPosition()
+	 * Gets the position of the ghosts on the board.
+	 * @return the ghosts position
+	 * @post int[].length() = 2 && 0 <= int[0] < boardWidth && 0 <= int[1] < boardHeight
 	 */
 	public EnumMap<GhostType, int[]> getGhostsPosition();
 	
 	/**
-	 * @see IDataLoader#getSpeed()
+	 * Gets the speed percent.
+	 * @return the speed percent
+	 * @post 200 >= speed >= 50
 	 */
 	public int getSpeed();
 	
 	/**
-	 * @see IDataLoader#getBestScore()
+	 * Gets the best score.
+	 * @return the best score
+	 * @post bestScore >= 0
 	 */
 	public int getBestScore();
 	
